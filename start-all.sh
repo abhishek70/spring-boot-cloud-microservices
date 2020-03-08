2c3b3757db38da0c2959635ed1f254d0200be6ad
Abhishek Desai <abhishek.desai70@gmail.com>
2020-03-08 16:42:24 -0700

    Added react-ui service in docker-compose file

---
+++ start-all.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Generate service JAR files
+#mvn clean install -DskipTests
+
+# Deleting network if available
+docker network rm spring-cloud-microservices
+
+# Creating network for services
+docker network create spring-cloud-microservices
+
+# Increasing default HTTP Timeout from 60 to 300
+export COMPOSE_HTTP_TIMEOUT=300
+
+# Start all services in background with -d flag
+docker-compose up -d
